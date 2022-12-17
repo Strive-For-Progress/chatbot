801b01c5caaf396fcb1a9b18a9451ea581759b1d
hsiehbingting <91364764+hsiehbingting@users.noreply.github.com>
2022-12-17 23:31:02 +0800

    Add files via upload
    更新database.py：
        修改list轉str的方式(原本：str(tuple(tags))，改為：'("' + '","'.join(tags) + '") '
        新增neg_tags、latest_projects功能

--- chatbot_ting/database.py
+++ chatbot_ting/database.py
@@ -5,18 +5,39 @@ db = pymysql.connect(host='mysql.cs.ccu.edu.tw',
                     password='xbt109u',
                     database='xbt109u_appdb')
 
-def tagToProjects(tags=[],user_id=-1,live=True,limit=-1):
-    #依據tags內容(match越多越優先)，列出user_id非PI、COPI的project
-    #不填user則直接依tags計算
-    if(len(tags)==1): tag_text = f'r.name = "{tags[0]}" '
-    elif(len(tags) == 0): return []
-    else: tag_text = f"r.name IN {str(tuple(tags))} "
+def tagToProjects(tags=[],neg_tags=[],user_id=-1,live=True,limit=5):
+    # 依據tags內容(match越多越優先)，列出user_id非PI、COPI的project
+    # 不填user則直接依tags計算 
+    # tags不填 回傳[] , neg_tags不填 依tags計算
+    # limit不填預設5
 
-    if(limit<=0): limit = 5 
-    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
+    if(len(tags) == 0): return []
+    else: tags_text =  '"' + '","'.join(tags) + '"'
+
+    if(limit <= 0): limit = 5 
 
     if live: live_text = "AND NOW() < sp.apply_copi_end_date "
     else: live_text = ""
+
+    if(len(neg_tags) == 0): neg_tags_text = ""
+    else:
+        neg_tags_text = (
+            "WHERE "
+            "match_table.id NOT IN ( "
+                "SELECT sp.id "
+                "FROM specific_periods sp "
+                "JOIN researches r "
+                "JOIN specific_period_researches spr "
+                "WHERE "
+        ) 
+        neg_tags_text += 'r.name IN ("' + '","'.join(neg_tags) + '") '        
+        neg_tags_text += (
+                    "AND r.id = spr.research_id AND spr.specific_period_id = sp.id "
+                    "GROUP BY sp.id "
+            ") "
+        )
+
+    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)    
     sql=(
         "SELECT sp.id, sp.project_title, GROUP_CONCAT(r.name ORDER BY r.id ASC) AS researches "
         "FROM specific_periods sp "
@@ -28,7 +49,7 @@ def tagToProjects(tags=[],user_id=-1,live=True,limit=-1):
                 "INNER JOIN users u "
                 
                 "WHERE "
-                    f"{tag_text}"
+                    f"r.name IN ({tags_text}) "
                     "AND r.id = spr.research_id AND spr.specific_period_id = sp.id "
                     f"{live_text}"
                     "AND u.id = sp.user_id "
@@ -38,6 +59,7 @@ def tagToProjects(tags=[],user_id=-1,live=True,limit=-1):
                 ") match_table ON match_table.id = sp.id "
         "JOIN specific_period_researches spr ON spr.specific_period_id = sp.id "
         "JOIN researches r ON spr.research_id = r.id "
+        f"{neg_tags_text}"
         "GROUP BY sp.id "
         "ORDER BY match_num DESC "
         f"LIMIT 0, {limit} "
@@ -48,16 +70,20 @@ def tagToProjects(tags=[],user_id=-1,live=True,limit=-1):
 
     return results
 
-def user_prefer_project(user_id=-1,live=True,limit=-1):
-    #依據user_id偏好(match越多越優先)，列出user_id非PI、COPI的project
-    #user不填 ，回傳{} 
+def user_prefer_project(user_id=-1,live=True,limit=5):
+    # 依據user_id偏好(match越多越優先)，列出user_id非PI、COPI的project
+    # user不填 ，回傳{} 
     # (live=True:不包含過期(copi_end_time))，反之包含
+    # limit不填預設5
+
     if(user_id < 0): return {}
-    if(limit <0): limit = 5
-    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
+
+    if(limit < 0): limit = 5
 
     if live: live_text = "AND NOW() < sp.apply_copi_end_date "
     else: live_text = ""
+
+    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)    
     sql=(
         "SELECT sp.id, sp.project_title, GROUP_CONCAT(r.name ORDER BY r.id ASC) AS researches "
         "FROM specific_periods sp "
@@ -80,7 +106,6 @@ def user_prefer_project(user_id=-1,live=True,limit=-1):
                     "AND u.id = sp.user_id "
                     f"AND u.id != {user_id} AND {user_id} NOT IN (SELECT user_id FROM COPI_project CO WHERE CO.specific_period_id = sp.id) "
                 
-               
                 "GROUP BY sp.id "                 
                 ") match_num ON match_num.id = sp.id "
         "JOIN specific_period_researches spr ON spr.specific_period_id = sp.id "
@@ -95,14 +120,17 @@ def user_prefer_project(user_id=-1,live=True,limit=-1):
 
     return results
 
-def all_projects(live = True):
+def all_projects(live = True,limit=5):
     # 列出所有project、project的id、title、researches
     # (live=True:不包含過期(copi_end_time))，反之包含
+    # limit不填預設5
+
     if live: live_text = "AND NOW() < sp.apply_copi_end_date "
     else: live_text = ""
 
-    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
+    if(limit < 0): limit = 5
 
+    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
     sql = (
             "SELECT sp.id, sp.project_title "
                 ", GROUP_CONCAT(c.name ORDER BY c.id) AS researches "
@@ -112,7 +140,7 @@ def all_projects(live = True):
             f"{live_text}"
             "GROUP BY sp.id "
             "ORDER BY sp.id "
-            "LIMIT 0,5 "
+            f"LIMIT 0,{limit} "
         )
 
     cursor.execute(sql)
@@ -123,8 +151,8 @@ def all_projects(live = True):
 def all_researches():
     # 列出所有researches
     cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
-
     sql = "SELECT name FROM researches"
+
     cursor.execute(sql)
     results = cursor.fetchall()
 
@@ -136,9 +164,9 @@ def all_researches():
 
 def user_researches(user_id=-1):
     # 列出user_id的researches
-    if(user_id == -1): return [] # 不填user_id回傳[]
-    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
+    if(user_id < 0): return [] # 不填user_id回傳[]
 
+    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
     sql = (
         "SELECT r.name FROM researches r "
         "JOIN user_researches ur "
@@ -156,9 +184,9 @@ def user_researches(user_id=-1):
 
 def specific_period_researches(specific_period_id = -1):
     # 列出project_id的researches
-    if(specific_period_id == -1): return []    # 不填specific_period_id回傳[]
-    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
+    if(specific_period_id < 0): return []    # 不填specific_period_id回傳[]
 
+    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
     sql = (
         "SELECT r.name FROM researches r "
         "JOIN specific_period_researches spr "
@@ -175,13 +203,42 @@ def specific_period_researches(specific_period_id = -1):
 
     return researches
 
+def latest_projects(limit=5,live=True):
+    # 列出所有project、project的id、title、researches(照時間)
+    # (live=True:不包含過期(copi_end_time))，反之包含 
+    # limit不填預設5
+
+    if live: live_text = "AND NOW() < sp.apply_copi_end_date "
+    else: live_text = ""
+
+    if(limit <= 0): limit = 5
+
+    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
+    sql = (
+            "SELECT sp.id, sp.project_title "
+                ", GROUP_CONCAT(c.name ORDER BY c.id) AS researches "
+            "FROM specific_periods sp "
+            "JOIN specific_period_researches spr ON spr.specific_period_id = sp.id "
+            "JOIN researches c ON c.id = spr.research_id "
+            f"{live_text}"
+            "GROUP BY sp.id "
+            "ORDER BY sp.start_date DESC "
+            f"LIMIT 0,{limit} "
+        )
+
+    cursor.execute(sql)
+    results = cursor.fetchall()
+
+    return results
+
+
 if __name__ == "__main__":
     print("===================================")    
-    for project in tagToProjects(tags=['AI','C++'],user_id=-1,live=True,limit=-1):print(project)
+    for project in tagToProjects(tags=['C++','AI','DL'],neg_tags=['web'],user_id=-1,live=True): print(project)
     print("===================================")
-    for project in user_prefer_project(user_id=7,limit=-1): print(project)
+    for project in user_prefer_project(user_id=7, limit=2): print(project)
     print("===================================")
-    for project in all_projects(live=True): print(project)
+    for project in all_projects(live=False,limit=2): print(project)
     print("===================================")
     print(all_researches())
     print("===================================")
@@ -190,4 +247,6 @@ if __name__ == "__main__":
     print(specific_period_researches(specific_period_id=3))
     print("===================================")    
     print(all_researches())
+    print("===================================")    
+    for project in latest_projects(limit=2): print(project)
     
