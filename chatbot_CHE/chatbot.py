7549390ca6a81720ab9d9b9a955c65413e17971d
hami0211 <82814921+hami0211@users.noreply.github.com>
2022-12-19 13:53:53 +0800

    Update chatbot.py

--- chatbot_CHE/chatbot.py
+++ chatbot_CHE/chatbot.py
@@ -1,6 +1,7 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from database import *
 import re
 import pymysql
 
@@ -27,102 +28,10 @@ bot = ChatBot(
         'maximum_similarity_threshold': 0.90
         }
     ],
-    database_url='sqlite:///database.sqlite3',
+    database_uri='mysql+pymysql://xbt109u:xbt109u@mysql.cs.ccu.edu.tw/xbt109u_chatDB',
     read_only="true"
 )
 
-def all_projects(live = True,limit=5):
-    # 列出所有project、project的id、title、researches
-    # (live=True:不包含過期(copi_end_time))，反之包含
-    # limit不填預設5
-
-    if live: live_text = "AND NOW() < sp.apply_copi_end_date "
-    else: live_text = ""
-
-    if(limit < 0): limit = 5
-
-    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)
-    sql = (
-            "SELECT sp.id, sp.project_title "
-                ", GROUP_CONCAT(c.name ORDER BY c.id) AS researches "
-            "FROM specific_periods sp "
-            "JOIN specific_period_researches spr ON spr.specific_period_id = sp.id "
-            "JOIN researches c ON c.id = spr.research_id "
-            f"{live_text}"
-            "GROUP BY sp.id "
-            "ORDER BY sp.id "
-            f"LIMIT 0,{limit} "
-        )
-
-    cursor.execute(sql)
-    results = cursor.fetchall()
-
-    return results
-
-def tagToProjects(tags=[],neg_tags=[],user_id=-1,live=True,limit=5):
-    # 依據tags內容(match越多越優先)，列出user_id非PI、COPI的project
-    # 不填user則直接依tags計算 
-    # tags不填 回傳[] , neg_tags不填 依tags計算
-    # limit不填預設5
-
-    if(len(tags) == 0): return []
-    else: tags_text =  '"' + '","'.join(tags) + '"'
-
-    if(limit <= 0): limit = 5 
-
-    if live: live_text = "AND NOW() < sp.apply_copi_end_date "
-    else: live_text = ""
-
-    if(len(neg_tags) == 0): neg_tags_text = ""
-    else:
-        neg_tags_text = (
-            "WHERE "
-            "match_table.id NOT IN ( "
-                "SELECT sp.id "
-                "FROM specific_periods sp "
-                "JOIN researches r "
-                "JOIN specific_period_researches spr "
-                "WHERE "
-        ) 
-        neg_tags_text += 'r.name IN ("' + '","'.join(neg_tags) + '") '        
-        neg_tags_text += (
-                    "AND r.id = spr.research_id AND spr.specific_period_id = sp.id "
-                    "GROUP BY sp.id "
-            ") "
-        )
-
-    cursor = db.cursor(cursor=pymysql.cursors.DictCursor)    
-    sql=(
-        "SELECT sp.id, sp.project_title, GROUP_CONCAT(r.name ORDER BY r.id ASC) AS researches "
-        "FROM specific_periods sp "
-        "JOIN ( "
-                "SELECT sp.id, COUNT(distinct r.id) AS match_num "
-                "FROM researches r "
-                "INNER JOIN specific_period_researches spr "
-                "INNER JOIN specific_periods sp "
-                "INNER JOIN users u "
-                
-                "WHERE "
-                    f"r.name IN ({tags_text}) "
-                    "AND r.id = spr.research_id AND spr.specific_period_id = sp.id "
-                    f"{live_text}"
-                    "AND u.id = sp.user_id "
-                    f"AND u.id != {user_id} AND {user_id} NOT IN (SELECT user_id FROM COPI_project CO WHERE CO.specific_period_id = sp.id) "
-                
-                "GROUP BY sp.id " 
-                ") match_table ON match_table.id = sp.id "
-        "JOIN specific_period_researches spr ON spr.specific_period_id = sp.id "
-        "JOIN researches r ON spr.research_id = r.id "
-        f"{neg_tags_text}"
-        "GROUP BY sp.id "
-        "ORDER BY match_num DESC "
-        f"LIMIT 0, {limit} "
-    )
-
-    cursor.execute(sql)
-    results = cursor.fetchall()
-
-    return results
 
 def case1(input_text):
     split_message = re.split(r'\s+|[,;?!.-]\s*', input_text.lower()) #以 pattern 為分隔島, 將母字串分開
